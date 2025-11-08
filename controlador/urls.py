bffd060dbb9fec41804f8e6e4efe8402aeef6758
heiber0 <heiberrbrito@gmail.com>
2025-11-08 13:31:30 -0500

    Cambios propuestos en base al figma

--- controlador/urls.py
+++ controlador/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('task/', login_views.task, name='task'),
     path('logout/', login_views.signout, name='logout'),
     path('signin/', login_views.signin, name='signin'),
+    # NUEVA RUTA: Para cargar los detalles de la materia (e.g., /materia/algebra/)
+    path('materia/<str:materia_id>/', login_views.materia_detail, name='materia_detail'),
 ]
